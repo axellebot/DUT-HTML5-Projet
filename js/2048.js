3ab4d13ea333461dd0a14acb8c68a93654cd5d66
axellebot <axellebot@gmail.com>
2016-03-18 09:04:33 +0100

    new class

--- js/2048.js
+++ js/2048.js
@@ -14,3 +14,58 @@ $(document).on("touchmove", function (e) {
         console.log('moving up');
     }
 });
+
+function Case(posX, posY, color, n) {
+    this.x = posX;
+    this.y = posY;
+    this.color = color || "white";
+    this.valeur = n || null;
+}
+
+function Grille() {
+    this.taille = 4;
+    this.grille = new Array();
+
+    for (var i = 0; i < this.taille; i++) {
+        this.grille[i] = new Array();
+    }
+    for (var i = 0; i < this.taille; i++) {
+        for (var j = 0; j < taille; j++) {
+            console.log("i -> " + i);
+            console.log("j -> " + j);
+            this.grille[i][j] = new Case(j, i);
+        }
+    }
+
+    this.recup_colonne = function (j) {
+        var tab = new Array();
+        for (var i = 0; i < this.taille; i++) {
+            tab[i] = 0;
+        }
+        var k = 0;
+        for (var i = 0; i < this.taille; i++) {
+            if (grille[i][j].valeur > 0) {
+                tab[k] = grille[i][j].valeur;
+                k++;
+            }
+        }
+        return tab;
+    }
+    this.recup_colonne = function (i) {
+        var tab = new Array();
+        for (var i = 0; i < this.taille; i++) {
+            tab[i] = 0;
+        }
+        var k = 0;
+        for (var j = 0; j < this.taille; j++) {
+            if (grille[i][j].valeur > 0) {
+                tab[k] = grille[i][j].valeur;
+                k++;
+            }
+        }
+        return tab;
+    }
+}
+
+
+grille = new Grille();
